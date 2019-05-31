9ea8790970850b399fdf80c0535ef1d091bf1722
Kyle Silver <kyleAsilver@gmail.com>
2019-05-31 10:39:39 -0400

    corpy visual tweak

--- sbemail/screens/CorpyNT6.py
+++ sbemail/screens/CorpyNT6.py
@@ -7,10 +7,15 @@ class CorpyNT6(EmailScreen):
         self.sbemail = sbemail
         subject = sbemail['subject'] if sbemail['subject'] is not '' else sbemail['title']
         frame = urwid.Frame(
-            header=self.header(subject, palette='corpy_accent'),
+            header=self.header(subject),
             body=self.body(), 
             footer=self.footer(palette='corpy_accent'))
         return super(CorpyNT6, self).__init__(frame, parent)
+    
+    def header(self, subject):
+        text = urwid.Text(['Subject: ', subject])
+        text = urwid.AttrMap(text, 'corpy_accent')
+        return text
     
     def body(self):
         text = urwid.Text(self.sbemail['body'])
